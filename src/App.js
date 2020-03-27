d9f852a741998f5be8899e332f21016a13b2483f
nilold@gmail.com <Armadillo@9090>
2020-03-27 13:15:04 -0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
