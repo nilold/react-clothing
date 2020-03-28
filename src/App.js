ecc18194483f3af3b7bdadf4880ff9c5b5bb27ca
nilold@gmail.com <Armadillo@9090>
2020-03-28 18:58:41 -0300

    checkout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
-import {Switch, Route, Redirect} from "react-router-dom";
-import "./App.css";
-import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
-
+import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {setCurrentUser} from "./redux/user/user.actions";
+import {createStructuredSelector} from 'reselect';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
+
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+
+import {setCurrentUser} from './redux/user/user.actions';
+import {selectCurrentUser} from './redux/user/user.selectors';
 
 class App extends React.Component {
     unsubscribeFromAuth = null;
@@ -19,34 +26,41 @@ class App extends React.Component {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
-                userRef.onSnapshot(snapshot => {
+
+                userRef.onSnapshot(snapShot => {
                     setCurrentUser({
-                        currentUser: {
-                            id: snapshot.id,
-                            ...snapshot.data()
-                        }
-                    }, () => console.log(this.state.currentUser));
+                        id: snapShot.id,
+                        ...snapShot.data()
+                    });
                 });
-            } else {
-                setCurrentUser(userAuth);
             }
+
+            setCurrentUser(userAuth);
         });
     }
 
     componentWillUnmount() {
-        console.log("Unsubscribing!");
         this.unsubscribeFromAuth();
     }
 
     render() {
         return (
-            <div className="App">
+            <div>
                 <Header/>
                 <Switch>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route exact path="/shop" component={ShopPage}/>
-                    <Route exact path="/signin"
-                           render={() => this.props.currentUser ? <Redirect to={"/"}/> : < SignInAndSignUpPage/>}
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
+                    <Route
+                        exact
+                        path='/signin'
+                        render={() =>
+                            this.props.currentUser ? (
+                                <Redirect to='/'/>
+                            ) : (
+                                <SignInAndSignUpPage/>
+                            )
+                        }
                     />
                 </Switch>
             </div>
@@ -54,12 +68,15 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({user}) => ({
-    currentUser: user.currentUser
-})
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
