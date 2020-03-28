9de803d90af25f78558104872b0d9d4c310ee6f9
nilold@gmail.com <Armadillo@9090>
2020-03-28 16:30:00 -0300

    added cart reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,26 +3,26 @@ import SHOP_DATA from "./shop.data";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
 
-  render() {
-    const { collections } = this.state;
+    render() {
+        const {collections} = this.state;
 
-    return (
-      <div className="shopPage">
-        {collections
-          .map(({ id, ...otherProps }) => (
-            <CollectionPreview key={id} {...otherProps} />
-          ))}
-      </div>
-    );
-  }
+        return (
+            <div className="shopPage">
+                {collections
+                    .map(({id, ...otherProps}) => (
+                        <CollectionPreview key={id} {...otherProps} />
+                    ))}
+            </div>
+        );
+    }
 }
 
 export default ShopPage;
