cf837cedf559f4fa0afd5235ee32f399754ef32f
Nilo Serafim Neto <nilold@gmail.com>
2020-05-03 19:51:46 -0300

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyAY6KnMBRL0n33LWeCRyWz0g9wzMv-0eK0",
-  authDomain: "playground-270320.firebaseapp.com",
-  databaseURL: "https://playground-270320.firebaseio.com",
-  projectId: "playground-270320",
-  storageBucket: "playground-270320.appspot.com",
-  messagingSenderId: "95101430558",
-  appId: "1:95101430558:web:0e1b6846857fff5e31f70f",
-  measurementId: "G-LG6TDC95XG"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export const createUserProfileDocument = async (userAuth, aditionalData) => {
